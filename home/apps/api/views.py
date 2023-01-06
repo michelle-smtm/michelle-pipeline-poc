5ccf67ec4d64e93e593fb0f0771436000e43e45e
Michelle Li <michelle@daughterscapital.com>
2023-01-06 16:34:44 -0500

    Checking from v1

--- home/apps/api/views.py
+++ home/apps/api/views.py
@@ -20,4 +20,4 @@ class HelloWorldDeleteAPIView(generics.DestroyAPIView):
     serializer_class = HelloWorldSerializer 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello world! CodePipeline change v6"})
+    return JsonResponse({"message": "Hello world! CodePipeline change full CD v1"})
