04efe35abe6d7fb6f66bfc2c7d64828bc5100790
Michelle Li <michelle@daughterscapital.com>
2023-01-09 19:55:52 -0500

    Switching to hello cdk app

--- hello-cdk/app.py
+++ hello-cdk/app.py
@@ -13,7 +13,7 @@ PipelineStack(app, "PipelineStack",
     env=cdk.Environment(account='062621911729', region='us-east-1'),
 )
 
-# HelloCdkStack(app, "HelloCdkStack",
+HelloCdkStack(app, "HelloCdkStack",
 #     # If you don't specify 'env', this stack will be environment-agnostic.
 #     # Account/Region-dependent features and context lookups will not work,
 #     # but a single synthesized template can be deployed anywhere.
@@ -21,14 +21,13 @@ PipelineStack(app, "PipelineStack",
 #     # Uncomment the next line to specialize this stack for the AWS Account
 #     # and Region that are implied by the current CLI configuration.
 
-#     # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
 #     # Uncomment the next line if you know exactly what Account and Region you
 #     # want to deploy the stack to. */
-
-#     env=cdk.Environment(account='062621911729', region='us-east-1'),
+    env=cdk.Environment(account='062621911729', region='us-east-1'),
 
 #     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-#    )
+)
 
 app.synth()
